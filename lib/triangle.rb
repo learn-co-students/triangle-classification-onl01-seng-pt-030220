9ef6b8fbc4dfa226e2d53731e07f2dca81288c31
alelee93 <leevargasale@gmail.com>
2020-04-03 20:58:44 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  attr_accessor :l1, :l2, :l3
+
+  def initialize (l1, l2, l3)
+    @l1 = l1
+    @l2 = l2
+    @l3 = l3
+
+    if @l1 <= 0 || @l2 <= 0 || @l3 <=0
+      raise TriangleError 
+    elsif
+      @l1 + @l2 <= @l3 || @l2 + @l3 <= @l1 || @l1 + @l3 <= @l2
+      raise TriangleError
+    end
+  end
+
+  def kind
+    if @l1 == @l2 && @l2 == @l3
+      :equilateral
+    elsif @l1 != @l2 && @l2 != @l3 && @l3 != @l1
+      :scalene
+    else
+      :isosceles
+    end
+  end
+
+  class TriangleError < StandardError
+    puts  "size length isnt valid" 
+  end
+
 end
